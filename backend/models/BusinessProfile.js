fac619cf715f65d48a9ba0ab85b7f891e8da0846
Jake Samuels <samuels242@googlemail.com>
2025-06-09 00:42:20 +0100

    BusinessProfile model

--- backend/models/BusinessProfile.js
+++ backend/models/BusinessProfile.js
@@ -1,45 +1,42 @@
-// import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
-// const businessProfileSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-//     logoUrl: {
-//       type: String,
-//     },
-//     description: {
-//       type: String,
-//       trim: true,
-//     },
-//     industry: {
-//       type: String,
-//     },
-//     ownerId: {
-//       type: Schema.Types.ObjectId,
-//       ref: "User",
-//     },
-//     isActive: {
-//       type: Boolean,
-//       default: true,
-//     },
-//     createdAt: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//     updatedAt: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//   },
-//   { timestamps: true, versionKey: false }
-// );
+const businessProfileSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logoUrl: {
+      type: String,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    industry: {
+      type: String,
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
 
-// const BusinessProfile = model(
-//   "BusinessProfile",
-//   businessProfileSchema
-// );
+const BusinessProfile = model("BusinessProfile", businessProfileSchema);
 
-// export default BusinessProfile;
+export default BusinessProfile;
