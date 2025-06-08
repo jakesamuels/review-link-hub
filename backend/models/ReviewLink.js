8e249f528ac1e68aad3473b423710ee3e6af6dbb
Jake Samuels <samuels242@googlemail.com>
2025-06-09 00:39:53 +0100

    Design ReviewLink Schema

---
+++ backend/models/ReviewLink.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+
+const reviewLinkSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return validator.isURL(value);
+        },
+        message: "Please provide a valid URL",
+      },
+    },
+    siteName: {
+      type: String,
+      required: true,
+    },
+    businessProfileId: {
+      type: Schema.Types.ObjectId,
+      ref: "BusinessProfile",
+    },
+    order: {
+      type: Number,
+    },
+    clickCount: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const ReviewLink = model("ReviewLink", reviewLinkSchema);
+
+export default ReviewLink;
