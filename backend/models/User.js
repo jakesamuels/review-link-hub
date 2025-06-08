880cb302c9587253da5d18450d6e93ed665103e5
Jake Samuels <samuels242@googlemail.com>
2025-06-09 00:41:38 +0100

    User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,53 +1,53 @@
-// import { Schema, model } from "mongoose";
-// import validator from "validator";
+import { Schema, model } from "mongoose";
+import validator from "validator";
 
-// const userSchema = new Schema(
-//   {
-//     email: {
-//       type: String,
-//       required: [true, "Email must be provided."],
-//       unique: true,
-//       trim: true,
-//       lowercase: true,
-//       validate: {
-//         validator: function (value) {
-//           return validator.isEmail(value);
-//         },
-//         message: "Please provide a valid email",
-//       },
-//     },
-//     businessProfileId: {
-//       type: Schema.Types.ObjectId,
-//       ref: "BusinessProfile",
-//     },
-//     password: {
-//       type: String,
-//       required: [true, "Password must be provided"],
-//     },
-//     passwordConfirm: {
-//       type: String,
-//       required: [true, "Please confirm your password"],
-//       validate: {
-//         // This only works on CREATE and SAVE!
-//         validator: function (el) {
-//           return el === this.password;
-//         },
-//         message: "Passwords do not match",
-//       },
-//     },
-//     passwordChangedAt: { type: Date },
-//     createdAt: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//     updatedAt: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//   },
-//   { timestamps: true, versionKey: false }
-// );
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email must be provided."],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (value) {
+          return validator.isEmail(value);
+        },
+        message: "Please provide a valid email",
+      },
+    },
+    businessProfileId: {
+      type: Schema.Types.ObjectId,
+      ref: "BusinessProfile",
+    },
+    password: {
+      type: String,
+      required: [true, "Password must be provided"],
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        // This only works on CREATE and SAVE!
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords do not match",
+      },
+    },
+    passwordChangedAt: { type: Date },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
 
-// const User = model("User", userSchema);
+const User = model("User", userSchema);
 
-// export default User;
+export default User;
