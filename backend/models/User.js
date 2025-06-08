500aec266216a7c396a4321cadd21f64a1e98696
Jake Samuels <samuels242@googlemail.com>
2025-06-09 00:21:19 +0100

    Design User Schema

---
+++ backend/models/User.js
@@ -0,0 +1,50 @@
+// import { mongoose, Schema } from "mongoose";
+// import validator from "validator";
+
+// const userSchema = new Schema(
+//   {
+//     email: {
+//       type: String,
+//       required: [true, "Email must be provided."],
+//       unique: true,
+//       trim: true,
+//       lowercase: true,
+//       validate: [validator.isEmail, "Please provide a valid email"],
+//     },
+//     businessProfileId: {
+//       id: {
+//         type: Schema.Types.ObjectId,
+//         ref: "BusinessProfile",
+//       },
+//     },
+//     password: {
+//       type: String,
+//       required: [true, "Password must be provided"],
+//     },
+//     passwordConfirm: {
+//       type: String,
+//       required: [true, "Please confirm your password"],
+//       validate: {
+//         // This only works on CREATE and SAVE!
+//         validator: function (el) {
+//           return el === this.password;
+//         },
+//         message: "Passwords do not match",
+//       },
+//     },
+//     passwordChangedAt: { type: Date },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//     },
+//     updatedAt: {
+//       type: Date,
+//       default: Date.now,
+//     },
+//   },
+//   { timestamps: true, versionKey: false }
+// );
+
+// const User = new mongoose.model("User", userSchema);
+
+// export default User;
