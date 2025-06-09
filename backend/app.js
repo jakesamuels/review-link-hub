f555d79187103415a96f13d997b7173103207556
Jake Samuels <samuels242@googlemail.com>
2025-06-09 14:51:50 +0100

    Import and use global error handler

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+
+import AppError from "./utils/appError.js";
+import globalErrorHandler from "./controllers/errorController.js";
 import authRouter from "./routes/authRoutes.js";
 
 const app = express();
@@ -10,5 +13,11 @@ app.use(express.json());
 express.urlencoded({ extended: true });
 
 app.use("/api/auth", authRouter);
+
+app.all("/{*splat}", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 400));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
