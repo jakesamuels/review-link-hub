4b4cb69fb29183ac1cd7a7ee5cfd85041646dc22
Jake Samuels <samuels242@googlemail.com>
2025-06-09 13:23:55 +0100

    Implement Authentication Middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,4 @@
+import promisify from "util";
 import User from "./../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -91,6 +92,54 @@ export const loginUser = async (req, res, next) => {
     }
 
     createSendToken(user, 200, res);
+  } catch (error) {
+    console.error(error.message || error);
+    res.status(400).json({
+      status: "fail",
+      message: error.message || "Something went wrong!",
+    });
+  }
+};
+
+export const protect = async (req, res, next) => {
+  try {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    } else if (req.cookies.jwt) {
+      token = req.cookies.jwt;
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        status: "fail",
+        message: "You are not logged in! Please log in to get access",
+      });
+    }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const currentUser = await User.findById(decoded.id).select("-password");
+    if (!currentUser) {
+      return res.status(401).json({
+        status: "fail",
+        message: "The user belonging to this token no longer exists",
+      });
+    }
+
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return res.status(401).json({
+        status: "fail",
+        message: "User recently changed password! Please log in again.",
+      });
+    }
+
+    req.user = currentUser;
+    res.locals.user = currentUser;
+    next();
   } catch (error) {
     console.error(error.message || error);
     res.status(400).json({
