1a543f2adc4533ac5051258fc37c5915d2f98829
Jake Samuels <samuels242@googlemail.com>
2025-06-09 13:57:47 +0100

    Remove origin from cors (temporary)

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 
 app.use(express.json());
 express.urlencoded({ extended: true });
