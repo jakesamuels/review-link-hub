8d9e204c79144d28aa8f6942cb6a0b1db79f9e0f
Jake Samuels <samuels242@googlemail.com>
2025-06-09 13:23:05 +0100

    Correct app() location

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
 const app = express();
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.json());
 express.urlencoded({ extended: true });
