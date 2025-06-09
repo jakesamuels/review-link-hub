82d81aac3a6cdeacb92183e46c9d9a14d0360ef3
Jake Samuels <samuels242@googlemail.com>
2025-06-09 01:54:07 +0100

    Implement User Login Logic

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,51 @@
 import User from "./../models/User.js";
+import jwt from "jsonwebtoken";
+
+const signToken = (id) => {
+  return jwt.sign(
+    {
+      id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 export const registerUser = async (req, res, next) => {
   try {
     const { email, password, passwordConfirm } = req.body;
 
     if (!email || !password || !passwordConfirm) {
-      throw new Error("Please fill in all the required fields");
+      return res.status(400).json({
+        status: "fail",
+        message: "Please fill in all the required fields",
+      });
     }
 
     const existingUser = await User.findOne({ email });
@@ -20,14 +60,37 @@ export const registerUser = async (req, res, next) => {
     const newUser = new User({ email, password, passwordConfirm });
     await newUser.save();
 
-    const { password: _, ...userWithoutPassword } = newUser.toObject();
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        user: userWithoutPassword,
-      },
+    createSendToken(newUser, 201, res);
+  } catch (error) {
+    console.error(error.message || error);
+    res.status(400).json({
+      status: "fail",
+      message: error.message || "Something went wrong!",
     });
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Please fill in all the required fields",
+      });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password))) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Incorrect email or password",
+      });
+    }
+
+    createSendToken(user, 200, res);
   } catch (error) {
     console.error(error.message || error);
     res.status(400).json({
