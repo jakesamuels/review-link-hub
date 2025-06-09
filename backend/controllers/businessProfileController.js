533520521033a20cd51d2572ebf0185d82811f2f
Jake Samuels <samuels242@googlemail.com>
2025-06-09 15:51:49 +0100

    Implement Update Business Profile Logic

--- backend/controllers/businessProfileController.js
+++ backend/controllers/businessProfileController.js
@@ -29,7 +29,7 @@ export const createBusinessProfile = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: "success",
     data: {
-      businessProfile,
+      profile: businessProfile,
     },
   });
 });
@@ -53,7 +53,7 @@ export const getBusinessProfileById = catchAsync(async (req, res, next) => {
   res.status(200).json({
     message: "success",
     data: {
-      businessProfile,
+      profile: businessProfile,
     },
   });
 });
@@ -73,8 +73,50 @@ export const getLoggedInUserBusinessProfile = catchAsync(
     res.status(200).json({
       message: "success",
       data: {
-        businessProfile,
+        profile: businessProfile,
       },
     });
   }
 );
+
+export const updateBusinessProfile = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, logoUrl, description, industry } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError("Invalid profile ID", 400));
+  }
+
+  const businessProfile = await BusinessProfile.findById(id);
+
+  if (!businessProfile) {
+    return next(new AppError("No profile found", 404));
+  }
+
+  if (businessProfile.ownerId.toString() !== req.user._id.toString()) {
+    return next(
+      new AppError(
+        "You are not authorised to update this profile! Please log in.",
+        403
+      )
+    );
+  }
+
+  const updatedProfile = await BusinessProfile.findByIdAndUpdate(
+    id,
+    {
+      name,
+      logoUrl,
+      description,
+      industry,
+    },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      profile: updatedProfile,
+    },
+  });
+});
