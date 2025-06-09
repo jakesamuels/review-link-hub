a2405677d68971bceff0576ddb439d57c3219122
Jake Samuels <samuels242@googlemail.com>
2025-06-09 13:58:21 +0100

    Reconfig auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { registerUser, loginUser } from "./../controllers/authController.js";
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
