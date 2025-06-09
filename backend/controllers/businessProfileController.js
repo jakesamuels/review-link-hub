7304b3526c0f11dc69a481847a0a70c2b7e2b378
Jake Samuels <samuels242@googlemail.com>
2025-06-09 14:55:30 +0100

    Create Business Profile Controller File

---
+++ backend/controllers/businessProfileController.js
@@ -0,0 +1,6 @@
+import BusinessProfile from "./../models/BusinessProfile.js";
+import User from "./../models/User.js";
+import catchAsync from "./../utils/catchAsync.js";
+import AppError from "./../utils/appError.js";
+
+export const createBusinessProfile = async () => {};
