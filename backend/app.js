71e4bd5cb5732ac230c05f11380be12d2bfa39ea
Jake Samuels <samuels242@googlemail.com>
2025-06-09 02:01:01 +0100

    Import and use authRouter

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 import express from "express";
+import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 const app = express();
+
+app.use(express.json());
+express.urlencoded({ extended: true });
+
+app.use("/api/auth", authRouter);
 
 export default app;
