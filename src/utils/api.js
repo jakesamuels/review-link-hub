f37ff9c3945af5f933238c8f85efd3d47e20fa97
Jake Samuels <samuels242@googlemail.com>
2025-06-10 16:38:34 +0100

    send requests with credentials

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
-// For iOS
-export const baseURL =
-  "http://localhost:8000/api"; /* "http://10.0.2.2:3000" for android  */
+axios.defaults.withCredentials = true;
+
+// Set baseURL depending on the platform
+// Use http://10.0.2.2:8000 for Android emulator, http://localhost:8000 for iOS or physical devices on the same network
+export const baseURL = "http://localhost:8000/api";
+
+// Create an Axios instance
+const api = axios.create({
+  baseURL,
+  timeout: 10000, // optional: sets a 10-second timeout
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Export the Axios instance
+export default api;
