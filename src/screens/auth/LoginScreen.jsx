56aed9cf588fb60c390434914121853429b43944
Jake Samuels <samuels242@googlemail.com>
2025-06-10 16:47:56 +0100

    Integrate Login User Functionality

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   View,
   Text,
@@ -6,25 +7,86 @@ import {
   SafeAreaView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import api from "../../utils/api";
+import { setToken } from "../../utils/authStorage";
 
 const LoginScreen = () => {
   const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    if (!email || !password) {
+      setErrorMessage("Please fill in all fiends.");
+      setIsLoading(false);
+      return;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+      setErrorMessage("Please enter a valid email address.");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      const token = response.data?.token || response.token;
+
+      if (token) {
+        await setToken(token);
+        console.log("Login successful:", response.data);
+
+        navigation.navigate("AppNavigator", { screen: "Home" });
+      } else {
+        setErrorMessage(
+          "Login completed, but no token received. Please try logging in again."
+        );
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      error.response?.data?.message ||
+        error.message ||
+        "Login failed. Please try again.";
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <SafeAreaView>
       <Text>Log In</Text>
 
-      <TextInput placeholder="example@email.com" />
-      <TextInput placeholder="Password" />
+      <TextInput
+        placeholder="example@email.com"
+        onChangeText={setEmail}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry
+      />
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleLogin} disabled={isLoading}>
         <Text>Log In</Text>
       </TouchableOpacity>
 
       <View>
         <Text>Don't have an account? </Text>
         <TouchableOpacity>
-          <Text onPress={() => navigation.navigate("register")}>Register</Text>
+          <Text onPress={() => navigation.navigate("Register")}>Register</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
