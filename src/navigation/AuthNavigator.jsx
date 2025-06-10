a6e577f80b577b69b5d69a5511b31dc66954eb72
Jake Samuels <samuels242@googlemail.com>
2025-06-10 13:58:44 +0100

    Set up initial AuthNavigator

---
+++ src/navigation/AuthNavigator.jsx
@@ -0,0 +1,31 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="login">
+      <Stack.Screen
+        name="login"
+        component={LoginScreen}
+        options={{
+          headerShown: false,
+          path: "login",
+        }}
+      />
+
+      <Stack.Screen
+        name="register"
+        component={RegisterScreen}
+        options={{
+          title: "Register Account",
+          path: "register",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
