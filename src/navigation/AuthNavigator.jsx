67896593dc0a9f8e10db8fa0cf32e63e0cbb09b9
Jake Samuels <samuels242@googlemail.com>
2025-06-10 15:06:43 +0100

    Move auth screens to auth folder

--- src/navigation/AuthNavigator.jsx
+++ src/navigation/AuthNavigator.jsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "../screens/LoginScreen";
-import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
 
 const Stack = createStackNavigator();
 
